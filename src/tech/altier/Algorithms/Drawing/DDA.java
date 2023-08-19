be83af87b58d8ece2b1397c1374c26fb411aea4b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:46:51 +0530

    Update DDA.java

--- src/tech/altier/Algorithms/Drawing/DDA.java
+++ src/tech/altier/Algorithms/Drawing/DDA.java
@@ -1,8 +1,10 @@
 package tech.altier.Algorithms.Drawing;
 
+import tech.altier.Algorithms.Drawing.lib.Point;
+
 public class DDA {
     // This algorithm is used to draw a line between two points using the DDA algorithm
-    private static  dda(int x1, int y1, int x2, int y2) {
+    private static Point[] dda(int x1, int y1, int x2, int y2) {
 
     }
 }
