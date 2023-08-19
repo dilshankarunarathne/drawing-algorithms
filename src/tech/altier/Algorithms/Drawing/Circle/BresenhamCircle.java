28a0bfc0fe729f92a69b3b11549b3acabd1ee42a
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 21:32:40 +0530

    Update BresenhamCircle.java

--- src/tech/altier/Algorithms/Drawing/Circle/BresenhamCircle.java
+++ src/tech/altier/Algorithms/Drawing/Circle/BresenhamCircle.java
@@ -2,6 +2,7 @@ package tech.altier.Algorithms.Drawing.Circle;
 
 /**
  *  This class implements the Bresenham Circle algorithm
+ * @author Dilshan M. Karunarathne 
  */
 public class BresenhamCircle {
 }
