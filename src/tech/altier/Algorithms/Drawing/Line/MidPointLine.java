dd82f7e99454af1b2ce977f03a879093ba5e8e61
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 21:27:50 +0530

    Update MidPointLine.java

--- src/tech/altier/Algorithms/Drawing/Line/MidPointLine.java
+++ src/tech/altier/Algorithms/Drawing/Line/MidPointLine.java
@@ -9,5 +9,51 @@ import tech.altier.Algorithms.Drawing.lib.Point;
  *  @version 1.0
  */
 public class MidPointLine {
-    public static Point[]
+    public static Point[] midPointLine(int x0, int y0, int x1, int y1) {
+        // Calculate the difference between the coordinates
+        int dx = x1 - x0;
+        int dy = y1 - y0;
+
+        // Calculate the absolute value of the difference
+        int dxAbs = Math.abs(dx);
+        int dyAbs = Math.abs(dy);
+
+        // Calculate the decision parameter
+        int p = 2 * dyAbs - dxAbs;
+
+        // Calculate the initial values of the coordinates
+        int x = x0;
+        int y = y0;
+
+        // Create an array of points to store the points in the line
+        Point[] points = new Point[dxAbs + 1];
+
+        // Set the first point
+        points[0] = new Point(x, y);
+
+        // Loop through the points
+        int i = 1;
+        while (x < x1) {
+            // If the decision parameter is less than 0
+            if (p < 0) {
+                // Calculate the next value of the decision parameter
+                p = p + 2 * dyAbs;
+            } else {
+                // Calculate the next value of the decision parameter
+                p = p + 2 * dyAbs - 2 * dxAbs;
+
+                // Increment y
+                y++;
+            }
+
+            // Increment x
+            x++;
+
+            // Calculate the next point
+            points[i++] = new Point(x, y);
+        }
+
+        // Return the array of points
+        return points;
+    }
 }
