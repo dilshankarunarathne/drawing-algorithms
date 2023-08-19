8e78f3ae5afd0f53d217c1c4c1003f1ada2c05f8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:52:47 +0530

    Update DDA.java

--- src/tech/altier/Algorithms/Drawing/DDA.java
+++ src/tech/altier/Algorithms/Drawing/DDA.java
@@ -8,8 +8,8 @@ public class DDA {
      * @see Point
      * @param x1    The x coordinate of the first point
      * @param y1    The y coordinate of the first point
-     * @param x2
-     * @param y2
+     * @param x2    The x coordinate of the second point
+     * @param y2    The y coordinate of the second point
      * @return
      */
     public static Point[] dda(int x1, int y1, int x2, int y2) {
