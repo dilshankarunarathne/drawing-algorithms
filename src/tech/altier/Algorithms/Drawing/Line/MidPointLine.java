e9fc9f0f5d3f9ea37f8447d13ea6715061a36cf4
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 21:26:27 +0530

    Update MidPointLine.java

--- src/tech/altier/Algorithms/Drawing/Line/MidPointLine.java
+++ src/tech/altier/Algorithms/Drawing/Line/MidPointLine.java
@@ -2,6 +2,8 @@ package tech.altier.Algorithms.Drawing.Line;
 
 /**
  *  This class represents the Mid-Point Line algorithm
+ *  @see tech.altier.Algorithms.Drawing.lib.Point
+ *  
  */
 public class MidPointLine {
 }
