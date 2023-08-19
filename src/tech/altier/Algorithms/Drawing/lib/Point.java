cc07a40734a722857f471421635bc23d48aaa37c
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:39:08 +0530

    Update Point.java

--- src/tech/altier/Algorithms/Drawing/lib/Point.java
+++ src/tech/altier/Algorithms/Drawing/lib/Point.java
@@ -1,4 +1,8 @@
 package tech.altier.Algorithms.Drawing.lib;
 
 public class Point {
+    // This method is used to print the coordinates of the points to the terminal
+    public static void printPoints () {
+
+    }
 }
