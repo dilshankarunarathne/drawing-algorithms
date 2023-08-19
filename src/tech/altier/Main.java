703530c94f6eadf9498931226fedc9998169c942
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:50:03 +0530

    Update Main.java

--- src/tech/altier/Main.java
+++ src/tech/altier/Main.java
@@ -1,7 +1,9 @@
 package tech.altier;
 
+import static tech.altier.Algorithms.Drawing.lib.Points.printPoints;
+
 public class Main {
     public static void main(String[] args) {
-
+        printPoints(dda(0, 0, 10, 10));
     }
 }
