fa595ea0d224428aba6eb81873c8ab8dac3bfbdf
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 21:17:48 +0530

    Update BresenhamLine.java

--- src/tech/altier/Algorithms/Drawing/Line/BresenhamLine.java
+++ src/tech/altier/Algorithms/Drawing/Line/BresenhamLine.java
@@ -2,7 +2,8 @@ package tech.altier.Algorithms.Drawing.Line;
 
 /**
  *  This class represents the Bresenham Line algorithm
- *  
+ * @see tech.altier.Algorithms.Drawing.lib.Point
+ * 
  */
 public class BresenhamLine {
 
