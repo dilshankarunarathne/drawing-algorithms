80ede76da0a284917cc59182d9ebce9d6219fddf
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:39:49 +0530

    Update Point.java

--- src/tech/altier/Algorithms/Drawing/lib/Point.java
+++ src/tech/altier/Algorithms/Drawing/lib/Point.java
@@ -2,7 +2,18 @@ package tech.altier.Algorithms.Drawing.lib;
 
 // This class represents the x, y coordinates of a point
 public class Point {
+    // This is the x coordinate of the point
+    private int x;
+    // This is the y coordinate of the point
+    private int y;
 
+    // This is the constructor for the Point class
+    public Point (int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    
 
     // This method is used to print the coordinates of the points to the terminal
     public static void printPoints () {
