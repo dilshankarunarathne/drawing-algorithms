f8308f96e4fd401b0f42e34e81909cb4d559c063
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:57:01 +0530

    Update MidPointCircle.java

--- src/tech/altier/Algorithms/Drawing/Circle/MidPointCircle.java
+++ src/tech/altier/Algorithms/Drawing/Circle/MidPointCircle.java
@@ -2,9 +2,9 @@ package tech.altier.Algorithms.Drawing.Circle;
 
 /**
  * This class represents the Mid Point Circle algorithm
+ * @author Dilshan M. Karunarathne
  * @version 1.0
  * @see tech.altier.Algorithms.Drawing.lib.Point
- * @author Dilshan M. Karunarathne
  */
 public class MidPointCircle {
 }
