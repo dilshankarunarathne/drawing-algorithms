daa5fcbdc2e8d95f784693f955edc9a531a5a744
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:40:07 +0530

    Update Point.java

--- src/tech/altier/Algorithms/Drawing/lib/Point.java
+++ src/tech/altier/Algorithms/Drawing/lib/Point.java
@@ -13,7 +13,21 @@ public class Point {
         this.y = y;
     }
 
-    
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 
     // This method is used to print the coordinates of the points to the terminal
     public static void printPoints () {
