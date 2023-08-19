853d6d101fc4733723288ac3298f64de05940077
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 21:28:41 +0530

    Update Main.java

--- src/tech/altier/Main.java
+++ src/tech/altier/Main.java
@@ -2,10 +2,11 @@ package tech.altier;
 
 import static tech.altier.Algorithms.Drawing.Circle.MidPointCircle.midPointCircle;
 import static tech.altier.Algorithms.Drawing.Line.BresenhamLine.bresenhamLine;
+import static tech.altier.Algorithms.Drawing.Line.MidPointLine.midPointLine;
 import static tech.altier.Algorithms.Drawing.lib.Points.printPoints;
 
 public class Main {
     public static void main(String[] args) {
-        printPoints(bresenhamLine(4, 7, 14, 13));
+        printPoints(midPointLine(4, 7, 14, 13));
     }
 }
