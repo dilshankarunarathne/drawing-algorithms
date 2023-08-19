b196239b9ab24a4ce6ae7ecd1fc88b6a2c848ec5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:39:06 +0530

    Update Main.java

--- src/tech/altier/Main.java
+++ src/tech/altier/Main.java
@@ -4,9 +4,4 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-
-    // This method is used to print the coordinates of the points to the terminal
-    private static void printPoints () {
-
-    }
 }
