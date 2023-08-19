303d2647079dcbddf2c062b176e3ff5e192c85f5
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-19 11:48:58 +0530

    Update Points.java

--- src/tech/altier/Algorithms/Drawing/lib/Points.java
+++ src/tech/altier/Algorithms/Drawing/lib/Points.java
@@ -1,6 +1,10 @@
 package tech.altier.Algorithms.Drawing.lib;
 
 public class Points {
+    /**
+     * This method prints the points in the array
+     * @param points
+     */
     public static void printPoints(Point[] points) {
         for (Point point : points) {
             System.out.println(point);
