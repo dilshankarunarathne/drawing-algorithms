970b7b5ce2210a8f0953a305f4224bd200ef7ab7
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-20 23:07:35 +0530

    Update BresenhamCircle.java

--- src/BresenhamCircle.java
+++ src/BresenhamCircle.java
@@ -1,47 +1,10 @@
-import tech.altier.Algorithms.Drawing.lib.Point;
-
 public class BresenhamCircle {
-    public static Point[] bresenhamCircle(int x0, int y0, int r) {
-        // Calculate the first point
-        int x = 0;
-        int y = r;
+    public static void main(String[] args) {
 
-        // Create an array of points to store the points in the circle
-        Point[] points = new Point[4 * r];
+    }
 
-        // Calculate the initial value of the decision parameter
-        int d = 3 - (2 * r);
+    public static void bresenhamCircle(int x0, int y0, int r) {
 
-        // Set the first point
-        points[0] = new Point(x + x0, y + y0);
-
-        // Loop through the points
-        int i = 1;
-        while (y >= x) {
-            // If the decision parameter is less than 0
-            if (d < 0) {
-                // Calculate the next value of the decision parameter
-                d = d + (4 * x) + 6;
-            } else {
-                // Calculate the next value of the decision parameter
-                d = d + (4 * (x - y)) + 10;
-
-                // Decrement y
-                y--;
-            }
-
-            // Increment x
-            x++;
-
-            // Check if the index is within the bounds of the array
-            if (i < points.length) {
-                // Calculate the next point
-                points[i++] = new Point(x + x0, y + y0);
-            }
-        }
-
-        // Return the array of points
-        return points;
     }
 
     private static void drawCircle(int x0, int y0, int x, int y) {
