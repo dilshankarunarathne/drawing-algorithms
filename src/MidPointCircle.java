edde5474c11ffed9d3d2addea3eacc2c0f280d6f
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-20 23:12:03 +0530

    Update MidPointCircle.java

--- src/MidPointCircle.java
+++ src/MidPointCircle.java
@@ -2,4 +2,27 @@ public class MidPointCircle {
     public static void main(String[] args) {
         midPointCircle(4, 7, 4);
     }
+
+    public static void midPointCircle(int x0, int y0, int r) {
+        int x = 0;
+        int y = r;
+
+        int d = 1 - r;
+
+        drawCircle(x0, y0, x, y);
+
+        while (y > x) {
+            if (d < 0) {
+                d = d + (2 * x) + 3;
+            } else {
+                d = d + (2 * (x - y)) + 5;
+
+                y--;
+            }
+
+            x++;
+
+            drawCircle(x0, y0, x, y);
+        }
+    }
 }
