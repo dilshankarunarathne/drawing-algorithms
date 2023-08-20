c084e798922133fcd8bd1c7a4d141581c5c574b8
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-20 23:11:37 +0530

    Update BresenhamCircle.java

--- src/BresenhamCircle.java
+++ src/BresenhamCircle.java
@@ -27,7 +27,6 @@ public class BresenhamCircle {
     }
 
     private static void drawCircle(int x0, int y0, int x, int y) {
-        System.out.println("Drawing once...");
         System.out.println("(" + (x + x0) + ", " + (y + y0) + ")");
         System.out.println("(" + (y + x0) + ", " + (x + y0) + ")");
         System.out.println("(" + (-x + x0) + ", " + (y + y0) + ")");
