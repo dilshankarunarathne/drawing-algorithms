ba4777a4a1f0fc2ff5773b543aafdb092fbd0a1b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-20 23:12:10 +0530

    Update MidPointCircle.java

--- src/MidPointCircle.java
+++ src/MidPointCircle.java
@@ -25,4 +25,15 @@ public class MidPointCircle {
             drawCircle(x0, y0, x, y);
         }
     }
+
+    private static void drawCircle(int x0, int y0, int x, int y) {
+        System.out.println("(" + (x + x0) + ", " + (y + y0) + ")");
+        System.out.println("(" + (y + x0) + ", " + (x + y0) + ")");
+        System.out.println("(" + (-x + x0) + ", " + (y + y0) + ")");
+        System.out.println("(" + (-y + x0) + ", " + (x + y0) + ")");
+        System.out.println("(" + (-x + x0) + ", " + (-y + y0) + ")");
+        System.out.println("(" + (-y + x0) + ", " + (-x + y0) + ")");
+        System.out.println("(" + (x + x0) + ", " + (-y + y0) + ")");
+        System.out.println("(" + (y + x0) + ", " + (-x + y0) + ")");
+    }
 }
