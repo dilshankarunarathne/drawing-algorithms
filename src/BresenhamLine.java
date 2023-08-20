7a8e0c299d8eaa5931db7ea7ac48137b9918bd58
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-21 00:09:52 +0530

    Update BresenhamLine.java

--- src/BresenhamLine.java
+++ src/BresenhamLine.java
@@ -57,7 +57,7 @@ public class BresenhamLine {
 
                 System.out.println("(" + x + ", " + y + ")");
             }
-        } else if (y1 < y0) { // negative slope and x1 < x0
+        } else { // negative slope and x1 < x0
             int y = y0;
 
             System.out.println("(" + x0 + ", " + y0 + ")");
