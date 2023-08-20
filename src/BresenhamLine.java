4825346a540ce5bdd67e04a0fa4eeff081236cbc
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-21 00:11:59 +0530

    Update BresenhamLine.java

--- src/BresenhamLine.java
+++ src/BresenhamLine.java
@@ -9,6 +9,7 @@ public class BresenhamLine {
         int y0 = scanner.nextInt();
         System.out.print("Enter x1: ");
         int x1 = scanner.nextInt();
+        System.out.print("Enter y1: ");
     }
 
     public static void bresenhamLine(int x0, int y0, int x1, int y1) {
