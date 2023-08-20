27b210f094f8565271caebd9549e288073f71884
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-20 23:14:26 +0530

    Update BresenhamLine.java

--- src/BresenhamLine.java
+++ src/BresenhamLine.java
@@ -1,37 +1,9 @@
 public class BresenhamLine {
     public static void main(String[] args) {
-        bresenhamLine(1, 1, 5, 4);
+        bresenhamLine(0, 1, 5, 9);
     }
 
-public static void bresenhamLine(int x0, int y0, int x1, int y1) {
-        int dx = Math.abs(x1 - x0);
+    public static void bresenhamLine(int x0, int y0, int x1, int y1) {
 
-        int dy = Math.abs(y1 - y0);
-
-        int x = x0;
-
-        int y = y0;
-
-        int p = (2 * dy) - dx;
-
-        int twoDy = 2 * dy;
-
-        int twoDyMinusDx = 2 * (dy - dx);
-
-        System.out.println("(" + x + ", " + y + ")");
-
-        while (x < x1) {
-            x++;
-
-            if (p < 0) {
-                p += twoDy;
-            } else {
-                y++;
-
-                p += twoDyMinusDx;
-            }
-
-            System.out.println("(" + x + ", " + y + ")");
-        }
     }
 }
