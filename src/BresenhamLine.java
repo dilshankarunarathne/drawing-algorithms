c143a3b2fabc47675a30fa571ca5e92710a06b2b
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-21 00:01:48 +0530

    Update BresenhamLine.java

--- src/BresenhamLine.java
+++ src/BresenhamLine.java
@@ -1,10 +1,10 @@
 public class BresenhamLine {
     public static void main(String[] args) {
-        bresenhamLine(0, 1, 5, 9);
+        bresenhamLine(2, 5, 12, 10);
     }
 
     public static void bresenhamLine(int x0, int y0, int x1, int y1) {
-        if (x1 >= x0 && y1 >= y0) { // 1st quadrant
+        if (x1 >= x0 && y1 >= y0) { // positive slope and x1 >= x0
             int dx = x1 - x0;
             int dy = y1 - y0;
 
@@ -17,7 +17,7 @@ public class BresenhamLine {
 
             System.out.println("(" + x0 + ", " + y0 + ")");
 
-            for (int x=x0; x<=x1; x++) {
+            for (int x=x0+1; x<=x1; x++) {
                 if (d > 0) {
                     y++;
                     d += incrNE;
@@ -27,7 +27,76 @@ public class BresenhamLine {
 
                 System.out.println("(" + x + ", " + y + ")");
             }
-        }
+        } else if (x1 >= x0 && y1 < y0) { // negative slope and x1 >= x0
+            int dx = x1 - x0;
+            int dy = y0 - y1;
 
+            int d = 2 * dy - dx;
+
+            int incrE = 2 * dy;
+            int incrNE = 2 * (dy - dx);
+
+            int y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            for (int x=x0+1; x<=x1; x++) {
+                if (d > 0) {
+                    y--;
+                    d += incrNE;
+                } else {
+                    d += incrE;
+                }
+
+                System.out.println("(" + x + ", " + y + ")");
+            }
+        } else if (x1 < x0 && y1 >= y0) { // positive slope and x1 < x0
+            int dx = x0 - x1;
+            int dy = y1 - y0;
+
+            int d = 2 * dy - dx;
+
+            int incrE = 2 * dy;
+            int incrNE = 2 * (dy - dx);
+
+            int y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            for (int x=x0-1; x>=x1; x--) {
+                if (d > 0) {
+                    y++;
+                    d += incrNE;
+                } else {
+                    d += incrE;
+                }
+
+                System.out.println("(" + x + ", " + y + ")");
+            }
+        } else if (x1 < x0 && y1 < y0) { // negative slope and x1 < x0
+            int dx = x0 - x1;
+            int dy = y0 - y1;
+
+            int d = 2 * dy - dx;
+
+            int incrE = 2 * dy;
+            int incrNE = 2 * (dy - dx);
+
+            int y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            for (int x=x0-1; x>=x1; x--) {
+                if (d > 0) {
+                    y--;
+                    d += incrNE;
+                } else {
+                    d += incrE;
+                }
+
+                System.out.println("(" + x + ", " + y + ")");
+            }
+
+        }
     }
 }
