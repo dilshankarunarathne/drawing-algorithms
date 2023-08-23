0d12646f7270fd761e3be83c71e1f85a50920fd3
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-23 11:41:52 +0530

    Update DDALine.java

--- src/DDALine.java
+++ src/DDALine.java
@@ -1,5 +1,3 @@
-import tech.altier.Algorithms.Drawing.lib.Point;
-
 import java.util.Scanner;
 
 public class DDALine {
@@ -19,5 +17,77 @@ public class DDALine {
     }
 
     private static void ddaLine(int x0, int y0, int x1, int y1) {
+        int dx = (x1 >= x0) ? (x1 - x0) : (x0 - x1);
+        int dy = (y1 >= y0) ? (y1 - y0) : (y0 - y1);
+
+        if (x1 >= x0 && y1 >= y0) { // positive slope and x1 >= x0
+            int steps = (dx >= dy) ? dx : dy;
+
+            float xInc = dx / (float) steps;
+            float yInc = dy / (float) steps;
+
+            float x = x0;
+            float y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            for (int i=0; i<steps; i++) {
+                x += xInc;
+                y += yInc;
+
+                System.out.println("(" + (int) Math.round(x) + ", " + (int) Math.round(y) + ")");
+            }
+        } else if (x1 >= x0) { // negative slope and x1 >= x0
+            int steps = (dx >= dy) ? dx : dy;
+
+            float xInc = dx / (float) steps;
+            float yInc = dy / (float) steps;
+
+            float x = x0;
+            float y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            for (int i=0; i<steps; i++) {
+                x += xInc;
+                y -= yInc;
+
+                System.out.println("(" + (int) Math.round(x) + ", " + (int) Math.round(y) + ")");
+            }
+        } else if (y1 >= y0) { // positive slope and x1 < x0
+            int steps = (dx >= dy) ? dx : dy;
+
+            float xInc = dx / (float) steps;
+            float yInc = dy / (float) steps;
+
+            float x = x0;
+            float y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            for (int i=0; i<steps; i++) {
+                x -= xInc;
+                y += yInc;
+
+                System.out.println("(" + (int) Math.round(x) + ", " + (int) Math.round(y) + ")");
+            }
+        } else { // negative slope and x1 < x0
+            int steps = (dx >= dy) ? dx : dy;
+
+            float xInc = dx / (float) steps;
+            float yInc = dy / (float) steps;
+
+            float x = x0;
+            float y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            for (int i = 0; i < steps; i++) {
+                x -= xInc;
+                y -= yInc;
+
+                System.out.println("(" + (int) Math.round(x) + ", " + (int) Math.round(y) + ")");
+            }
+        }
     }
 }
