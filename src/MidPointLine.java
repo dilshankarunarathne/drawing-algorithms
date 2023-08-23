89c5523b5b686f7089b20979a782f987e1edf563
dilshankarunarathne <45087212+dilshankarunarathne@users.noreply.github.com>
2023-08-23 11:46:58 +0530

    Update MidPointLine.java

--- src/MidPointLine.java
+++ src/MidPointLine.java
@@ -17,5 +17,101 @@ public class MidPointLine {
     }
 
     private static void midPointLine(int x0, int y0, int x1, int y1) {
+        int dx = (x1 >= x0) ? (x1 - x0) : (x0 - x1);
+        int dy = (y1 >= y0) ? (y1 - y0) : (y0 - y1);
+
+        if (x1 >= x0 && y1 >= y0) { // positive slope and x1 >= x0
+            int d = dy - (dx / 2);
+
+            int incrE = dy;
+            int incrNE = dy - dx;
+
+            int x = x0;
+            int y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            while (x < x1) {
+                if (d <= 0) {
+                    d += incrE;
+                    x++;
+                } else {
+                    d += incrNE;
+                    x++;
+                    y++;
+                }
+
+                System.out.println("(" + x + ", " + y + ")");
+            }
+        } else if (x1 >= x0) { // negative slope and x1 >= x0
+            int d = dy - (dx / 2);
+
+            int incrE = dy;
+            int incrNE = dy - dx;
+
+            int x = x0;
+            int y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            while (x < x1) {
+                if (d <= 0) {
+                    d += incrE;
+                    x++;
+                } else {
+                    d += incrNE;
+                    x++;
+                    y--;
+                }
+
+                System.out.println("(" + x + ", " + y + ")");
+            }
+        } else if (y1 >= y0) { // positive slope and x1 < x0
+            int d = dy - (dx / 2);
+
+            int incrE = dy;
+            int incrNE = dy - dx;
+
+            int x = x0;
+            int y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            while (x > x1) {
+                if (d <= 0) {
+                    d += incrE;
+                    x--;
+                } else {
+                    d += incrNE;
+                    x--;
+                    y++;
+                }
+
+                System.out.println("(" + x + ", " + y + ")");
+            }
+        } else { // negative slope and x1 < x0
+            int d = dy - (dx / 2);
+
+            int incrE = dy;
+            int incrNE = dy - dx;
+
+            int x = x0;
+            int y = y0;
+
+            System.out.println("(" + x0 + ", " + y0 + ")");
+
+            while (x > x1) {
+                if (d <= 0) {
+                    d += incrE;
+                    x--;
+                } else {
+                    d += incrNE;
+                    x--;
+                    y--;
+                }
+
+                System.out.println("(" + x + ", " + y + ")");
+            }
+        }
     }
 }
